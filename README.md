7f190b74259f2db8cf56ce9f16e93d204d2fbfc0
AnnaT7459 <141446349+AnnaT7459@users.noreply.github.com>
2023-10-05 18:13:40 -0500

    debugged while statement and removed some while statements

--- README.md
+++ README.md
@@ -3,8 +3,21 @@
 ## Description
 This password generator was created as if I were an employee with access to sensitive data. I wanted to randomly generate a password that is 8-128 characters, include uppercase and/or lowercase characters, numbers, and/or special characters so that I can create a strong password that provides greater security. I learned how to create global variables, call functions, while statements (with help from a fellow classmate), variables for user inputs and computer generated bank of answers. I also learned how to randomize the bank of answers for a custom password. I also came to an understanding of how Event Listeners work through this week of class and how to apply it in my project. 
 
+There was also a point where my laptop glitched out and removed the JavaScript file. A tutor was able to help me recover by cloning the project again, which I will now reference in the future!
+
 ## Installation 
 N/A
 
 ## Usage
+
+![alt text](./assets/images/Screenshot%202023-10-05%20180239.png)
+
+## Credits
+
+H. Burke (personal communication, October 3, 2023)
+M. Myers (personal communication - tutoring, October 4, 2023)
+J. Diehl TA (personal communication - Office Hours, October 5, 2023)
+"JavaScript Statements": w3 Schools https://www.w3schools.com/js/js_statements.asp
+
+## Deployment Link
 
--- assets/script.js
+++ assets/script.js
@@ -7,23 +7,25 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 // called the generate password function
 function writePassword() {
+  var password = generatePassword()
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
-  var password = generatePassword()
-}
-// created variable for password length and a while statement "multiple ifs" -Harmony Burke
+};
+// created variable for password length and a while statement "multiple ifs" -H. Burke
   function generatePassword() {
-    var userLength = prompt("How many characters would you like to include in your password. Choose a length between 8-128");
-    while ((userLength < 8) || (userLength < 128) || (isNaN(userLength) === true)); {
+    var userLength = parseInt (prompt("How many characters would you like to include in your password? Choose a length between 8-128"));
+      while ((userLength < 8) || (userLength > 128) || (isNaN(userLength) === true)) {
       // added error message if incorrect value is entered
-      alert("ERROR: please select a number between 8 and 128.")
-      userLength = prompt ("How many characters would you like to include in your password. Choose a length between 8-128")
-    }
+        alert("ERROR: please select a number between 8 and 128.");
+        userLength = prompt ("How many characters would you like to include in your password? Choose a length between 8-128");
+    };
   // created variable for user choices
-  var userLowercase = confirm("Would you like your password to include lowercase characters?");
-  var userUppercase = confirm("Would you like your password to include uppercase characters?");
-  var userNumbers = confirm("Would you like your passord to include numbers?");
-  var userSpecial = confirm("Would you like your password to include special characters?");
+  // added while statements for each variable
+  // removed while statements
+  var userLowercase = confirm ("Would you like your password to include lowercase characters? yes or no");
+  var userUppercase = confirm ("Would you like your password to include uppercase characters? yes or no");
+  var userNumbers = confirm ("Would you like your passord to include numbers? yes or no");
+  var userSpecial = confirm ("Would you like your password to include special characters? yes or no");
 
   // created bank of answers
     var lowercase = "abcdefghijklmnopqrstuvwxyz";
